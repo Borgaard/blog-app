e8d5abe2d0b392a0051ba3568fcac4422ea79bc8
nicoleborgaard@gmail.com <nicoleborgaard@gmail.com>
2014-12-05 11:17:21 -0800

    Reorganize file structure, server status OK

--- app.js
+++ app.js
@@ -12,17 +12,46 @@ app.use(methodOverride('_method'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
-//Listen on port 3000
-app.listen(3000);
+//Listen on port 8080
+app.listen(8080);
 
 //Inform about setup when server running
-console.log("Running on 3000!");
+console.log("Running on 8080!");
 
+app.get('/posts/new', function(req, res) {
+	//console.log(req.url)
+	res.render('/posts/new')
+});
+
+//Set up routes, requests and responses
 app.get('/', function(req, res) {
 	res.render('index');
 });
 
-app.get('/posts/new', function(req, res) {
-	console.log(req,url)
-	res.render('/posts/new')
-});
+app.get('/posts/:id', function(req, res) {
+	console.log("got to posts id!")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
